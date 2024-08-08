53934e47404da8ebd750434438d4e1f26610b910
linux-navho <linux.navho@gmail.com>
2024-08-08 20:14:42 +0000

    build

--- dist/vendure-config.js
+++ dist/vendure-config.js
@@ -2,6 +2,7 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 const core_1 = require("@vendure/core");
@@ -41,13 +42,16 @@ exports.config = {
         },
     },
     dbConnectionOptions: {
-        type: 'better-sqlite3',
-        // See the README.md "Migrations" section for an explanation of
-        // the `synchronize` and `migrations` options.
-        synchronize: false,
-        migrations: [path_1.default.join(__dirname, './migrations/*.+(js|ts)')],
-        logging: false,
-        database: path_1.default.join(__dirname, '../vendure.sqlite'),
+        // ...
+        type: 'postgres',
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT ? +process.env.DB_PORT : undefined,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        ssl: process.env.DB_CA_CERT ? {
+            ca: process.env.DB_CA_CERT,
+        } : undefined,
     },
     paymentOptions: {
         paymentMethodHandlers: [core_1.dummyPaymentHandler],
@@ -58,11 +62,25 @@ exports.config = {
     plugins: [
         asset_server_plugin_1.AssetServerPlugin.init({
             route: 'assets',
-            assetUploadDir: path_1.default.join(__dirname, '../static/assets'),
-            // For local dev, the correct value for assetUrlPrefix should
-            // be guessed correctly, but for production it will usually need
-            // to be set manually to match your production url.
-            assetUrlPrefix: IS_DEV ? undefined : 'https://www.my-shop.com/assets/',
+            assetUploadDir: process.env.ASSET_UPLOAD_DIR || path_1.default.join(__dirname, '../static/assets'),
+            // If the MINIO_ENDPOINT environment variable is set, we'll use
+            // Minio as the asset storage provider. Otherwise, we'll use the
+            // default local provider.
+            storageStrategyFactory: process.env.MINIO_ENDPOINT ? (0, asset_server_plugin_1.configureS3AssetStorage)({
+                bucket: 'vendure-assets',
+                credentials: {
+                    accessKeyId: (_a = process.env.MINIO_ACCESS_KEY) !== null && _a !== void 0 ? _a : '', // If MINIO_ACCESS_KEY is undefined, use an empty string
+                    secretAccessKey: (_b = process.env.MINIO_SECRET_KEY) !== null && _b !== void 0 ? _b : '',
+                },
+                nativeS3Configuration: {
+                    endpoint: process.env.MINIO_ENDPOINT,
+                    forcePathStyle: true,
+                    signatureVersion: 'v4',
+                    // The `region` is required by the AWS SDK even when using MinIO,
+                    // so we just use a dummy value here.
+                    region: 'eu-west-1',
+                },
+            }) : undefined,
         }),
         core_1.DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
         core_1.DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
@@ -86,7 +104,7 @@ exports.config = {
             port: 3000,
             adminUiConfig: {
                 apiHost: 'https://api.navhocreatives.store',
-                apiPort: 3000,
+                apiPort: 443,
             },
         }),
     ],
